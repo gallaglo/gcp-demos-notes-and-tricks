bef17df873fbb9c41bcf877fa769023fc086b94c
logangallagher <logan@nubedehelado.com>
2025-04-13 15:26:20 -0700

    remove last component reference

--- run/animation/frontend/tailwind.config.ts
+++ run/animation/frontend/tailwind.config.ts
@@ -55,10 +55,6 @@ export default {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		animation: {
-  			'accordion-down': 'accordion-down 0.2s ease-out',
-  			'accordion-up': 'accordion-up 0.2s ease-out'
   		}
   	}
   },
