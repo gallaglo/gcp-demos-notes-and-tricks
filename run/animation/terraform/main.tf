98b7486871a3ec7d13fbb9fe7e1588978eb01385
logangallagher <logan@nubedehelado.com>
2025-08-27 08:28:10 -0700

    update container image to install correct packages for newer debian version

--- run/animation/terraform/main.tf
+++ run/animation/terraform/main.tf
@@ -220,6 +220,7 @@ resource "google_cloud_run_v2_service" "animator" {
         value = "/run/secrets/key.json"
       }
 
+
       volume_mounts {
         name       = "service-account"
         mount_path = "/run/secrets"
